238deb465cc8b663e33a9d7fb07d737d19b3838a
Shaun Darius <e2275642@cmaisonnneuve.qc.ca>
2025-05-13 12:46:33 -0400

    creation de methode csv et json

--- utils.py
+++ utils.py
@@ -4,13 +4,26 @@ import csv
 import json
 
 def lire_csv(chemin):
-    """
-    Lire un fichier CSV et retourner la liste des lignes.
+    lignes = []
+    with open("fichier_csv", 'r', encoding='utf-8') as f:
+        lecteur = csv.reader(f)
+        for ligne in lecteur:
+            dictionnaire= {}
+            for i in range(len(ligne)):
+                dictionnaire[str(i + 1)] = ligne[i]
+            lignes.append(dictionnaire)
+        return lignes
+
+"""Lire un fichier CSV et retourner la liste des lignes.
     Chaque dictionnaire correspond à une ligne du fichier.
     """
-    pass
+pass
 
 def sauvegarder_json(data, chemin):
+
+    with open(chemin, mode='w', encoding='utf-8') as fichier:
+        json.dump(data, fichier)
+
     """
     Sauvegarder des données dans un fichier JSON.
     - data : un objet Python (par exemple, un dictionnaire ou une liste)
@@ -20,6 +33,9 @@ def sauvegarder_json(data, chemin):
     pass
 
 def ecrire_texte(contenu, chemin):
+    with open(chemin, mode='w', encoding='utf-8') as fichier:
+        fichier.write(contenu)
+
     """
     Écrire du texte brut dans un fichier texte (.txt).
     - contenu : texte à écrire
